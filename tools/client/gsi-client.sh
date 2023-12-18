4945c383a36e31a9657033eca4efd923ea498b4f
Ubuntu <jofergus@microsoft.com>
2023-12-18 19:11:03 +0000

    new script to gather support information from a linux lustre client.

---
+++ tools/client/gsi-client.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+command_divider()
+{
+        echo $(date +"%FT%T"): " " >>  ~/$clientgsidir/gsi_client.log
+        echo $(date +"%FT%T"): "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" >>  ~/$clientgsidir/gsi_client.log
+	echo $(date +"%FT%T"): "command: ${*}" >>  ~/$clientgsidir/gsi_client.log
+	echo $(date +"%FT%T"): " " >>  ~/$clientgsidir/gsi_client.log
+}
+
+clientgsidir="client-gsi-$(date +"%FT%T")"
+echo $clientgsidir
+cd ~/
+mkdir $clientgsidir
+cd $clientgsidir
+echo $(date +"%FT%T"): "Starting gsi_client.sh cpature." > gsi_client.log
+echo $(date +"%FT%T"): "client gsi dir: " $clientgsidir >> gsi_client.log
+command_divider "uname -a"
+uname -a |tee uname_a >> gsi_client.log
+if [ -f /etc/lsb-release ]
+then
+        command_divider "cat /etc/lsb-release"
+        cat /etc/lsb-release |tee lsb-release >> gsi_client.log
+fi
+if [ -f /etc/redhat-release ]
+then
+	command_divider "cat /etc/redhat-release"
+	cat /etc/redhat-release |tee redhat-release >> gsi_client.log
+fi
+command_divider "uptime; uptime -p"
+uptime |tee uptime >> gsi_client.log; uptime -p |tee -a uptime >> gsi_client.log
+command_divider "netstat -rn"
+netstat -rn |tee netstat_rn >> gsi_client.log
+command_divider "netstat -Wan"
+netstat -Wan |tee netstat_Wan >> gsi_client.log
+command_divider "ifconfig -a"
+ifconfig -a 2>&1 |tee ifconfig_a >> gsi_client.log
+
+if [ -f /usr/bin/lfs ]
+then
+	command_divider "lfs --version"
+	lfs --version |tee lfs_version >> gsi_client.log
+	command_divider "lfs df -h"
+	lfs df -h |tee lfs_df >> gsi_client.log
+	command_divider "lfs check all"
+	lfs check all 2>&1 |tee lfs_check_all >> gsi_client.log
+fi
+
+if [ -f /var/log/syslog ]
+then
+	cd /var/log
+	command_divider "cd /var/log; tail -30 syslog"
+	tail -30 syslog |tee >> ~/$clientgsidir/gsi_client.log
+	command_divider "cd /var/log; tar cvfz ~/$clientgsidir/syslog.tgz syslog*"
+	tar cvfz ~/$clientgsidir/syslog.tgz syslog* >> ~/$clientgsidir/gsi_client.log
+	cd ~/$clientgsidir
+fi
+
+sudo_works=`sudo -n uptime 2>&1 | grep "load" | wc -l`
+if [ $sudo_works == 1 ] 
+then
+	command_divider "sudo dmesg -T"
+	sudo dmesg -T > dmesg
+	command_divider "sudo sysctl -a"
+	sudo sysctl -a > sysctl
+	command_divider "sudo lnetctl stats show"
+	sudo lnetctl stats show |tee lnetctl_stats >> gsi_client.log
+	command_divider "sudo lctl dl -t"
+	sudo lctl dl -t |tee lctl_dl >> gsi_client.log
+else
+	command_divider "No sudo permission.  Several commands will not be caputed."
+fi
+command_divider "mount |egrep lustre; mount"
+mount |egrep lustre |tee mount >> gsi_client.log; mount |tee -a mount >> gsi_client.log
+if [ -f /etc/fstab ]
+then
+	command_divider "cat /etc/fstab |egrep lustre; cat /etc/fstab"
+	cat /etc/fstab |egrep lustre |tee fstab >> gsi_client.log; cat /etc/fstab |tee -a fstab >> gsi_client.log
+else
+	command_divider "No /etc/fstab file."
+fi
+
+local_lustre_mount=`mount |egrep "type lustre" |awk '{print $3}' |tail -1`
+if [ $local_lustre_mount ]
+then
+	for read_ahead_kb in `ls /sys/devices/virtual/bdi/lustrefs-*/read_ahead_kb`
+	do
+        	command_divider "cat $read_ahead_kb"
+	        echo -n "$read_ahead_kb: " >> read_ahead_kb
+		cat $read_ahead_kb |tee -a read_ahead_kb >> gsi_client.log
+	done
+fi
+
+if [ $read_ahead_kb ]
+then
+	echo "Further analysis required if read_ahead_kb is > 0" >> gsi_client.log
+else
+	command_divider "client does not contain a value for lustrefs read_ahead_kb.  All good."
+fi
+
+# local_lustre_mount=`mount |egrep "type lustre" |awk '{print $3}' |tail -1`
+if [ $local_lustre_mount ]
+then
+	for local_lustre_mount in `mount |egrep "type lustre" |awk '{print $3}'`
+	do 
+		command_divider "find $local_lustre_mount -type f -print0 |xargs -0 -n 1 lfs  hsm_state"
+		hsm_state_file=`echo "hsm_state"$local_lustre_mount |sed 's/\//_/g'`
+		echo $local_lustre_mount |tee $hsm_state_file >> gsi_client.log
+		find $local_lustre_mount -type f -print0 |xargs -0 -n 1 lfs hsm_state >> $hsm_state_file
+		number_of_files=`wc -l $hsm_state_file`
+		echo "Number of files: $number_of_files" |tee -a $hsm_state_file >> gsi_client.log
+
+	done
+else
+	command_divider "Cannot find local lustre mount point.  Unable to display hsm_state."
+fi
+
+chmod 666 *
+cd ..
+gsi_compressed=`echo $clientgsidir.tgz |sed 's/:/-/g'`
+tar cvfz $gsi_compressed $clientgsidir/ >/dev/null
+
