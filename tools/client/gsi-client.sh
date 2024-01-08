9bce360228c60d7a1356085c0d7751eb562a9f78
Ray Ferguson <jofergus@microsoft.com>
2024-01-08 11:52:09 -0500

    change 5

--- tools/client/gsi-client.sh
+++ tools/client/gsi-client.sh
@@ -159,8 +159,7 @@ main() {
 		else
 			command_divider "Cannot find local lustre mount point.  Unable to display hsm_state."
 		fi
-		pwd; whoami
-		chmod 666 *
+		sudo chmod 666 *
 		cd ..
 		gsi_compressed=`echo $clientgsidir.tgz |sed 's/:/-/g'`
 		tar cvfz $gsi_compressed $clientgsidir/ >/dev/null
