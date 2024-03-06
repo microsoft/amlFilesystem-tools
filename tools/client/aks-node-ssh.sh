b1748303a1f9c231ebf2fb62e586351a40769c6f
Ray Ferguson <jofergus@microsoft.com>
2024-03-06 10:03:19 -0500

    Add -- chroot /host /bin/bash to kubectl command.

--- tools/client/aks-node-ssh.sh
+++ tools/client/aks-node-ssh.sh
@@ -36,7 +36,10 @@ main() {
                     then
                         aks_node="${nodes_array[$array_index]}"
                         echo "Once connected to node $aks_node enter chroot /host /bin/bash"
-                        kubectl debug node/"${aks_node}" --image=ubuntu -it
+                        kubectl debug node/"${aks_node}" --image=ubuntu -it -- chroot /host /bin/bash
+                        # kubectl debug node/"${aks_node}" --image=ubuntu -it
+                        # Adding "-- chroot /host /bin/bash" to the kubectl command above 
+                        # so users to not have to enter it manually.  May need to remove this later.
                     fi
                 fi
             else
